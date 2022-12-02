27223fce4019b6fdfae9c0456364fc4c3a483db8
michael-welch00 <80782476+michael-welch00@users.noreply.github.com>
2022-12-02 15:12:33 +0100

    Updated version number

--- DFMGenerator_SharedCode/Grid.cs
+++ DFMGenerator_SharedCode/Grid.cs
@@ -15,7 +15,7 @@ namespace DFMGenerator_SharedCode
         /// <summary>
         /// Program name and version number (hard coded)
         /// </summary>
-        public static string VersionNumber { get { return "DFMGenerator v2.1.1"; } }
+        public static string VersionNumber { get { return "DFMGenerator v2.2.0"; } }
 
         // Grid data
         /// <summary>
