ba7d1df36baa95e6b002a28ad356fc29202ee87e
michael-welch00 <80782476+michael-welch00@users.noreply.github.com>
2023-07-24 17:48:32 +0200

    Code modified to prevent negative mode factors and stress shadow widths (bug fix B73) and to ensure stress shadow widths changing do not cause errors, by reverting from ResidualActive to Growing evolution stage if the deactivation probability becomes very small or zero (bug fix B74)

--- DFMGenerator_SharedCode/Grid.cs
+++ DFMGenerator_SharedCode/Grid.cs
@@ -15,7 +15,7 @@ namespace DFMGenerator_SharedCode
         /// <summary>
         /// Program name and version number (hard coded)
         /// </summary>
-        public static string VersionNumber { get { return "DFMGenerator v2.2.1"; } }
+        public static string VersionNumber { get { return "DFMGenerator v2.2.2"; } }
 
         // Grid data
         /// <summary>
