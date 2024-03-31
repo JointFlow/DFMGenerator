0b59a41ba7e0a6319846b8102c1d7a7c1fb20da7
joint <jointflow@jointflow.eu>
2024-03-31 22:19:31 +0200

    Merge latest changes from main

--- DFMGenerator_Ocean/DFMGeneratorPlugin.cs
+++ DFMGenerator_Ocean/DFMGeneratorPlugin.cs
@@ -9,7 +9,7 @@ namespace DFMGenerator_Ocean
     {
         public override string AppVersion
         {
-            get { return "2023.1"; }
+            get { return "2024.1"; }
         }
 
         public override string Author
